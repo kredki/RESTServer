951a932f4a38592a7c17c0e92be701f849a7985a
imie <31960565+kredki@users.noreply.github.com>
2018-05-06 21:42:00 +0200

    add query param //not finished

--- src/main/java/oceny/services/StudentService.java
+++ src/main/java/oceny/services/StudentService.java
@@ -3,15 +3,11 @@ package oceny.services;
 import oceny.dao.StudentDAO;
 import oceny.exceptions.JsonError;
 import oceny.exceptions.NotFoundException;
-import oceny.lists.StudentList;
 import oceny.resources.Student;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.Optional;
-import java.util.concurrent.CopyOnWriteArrayList;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -22,9 +18,21 @@ import javax.ws.rs.core.Response;
 public class StudentService {
     private final StudentDAO studentDAO = StudentDAO.getInstance();
 
+    private static final String defaultString = "";
+
     @GET
     @Path("/students")
-    public List<Student> getAllStudentsJson() { return studentDAO.getStudentsList(); }
+    public List<Student> getAllStudentsJson(@DefaultValue(defaultString) @QueryParam("firstname") String firstName,
+                                            @DefaultValue(defaultString) @QueryParam("lastname") String lastName,
+                                            @DefaultValue(defaultString) @QueryParam("birthday") String birthday,
+                                            @DefaultValue(defaultString) @QueryParam("birthdayfrom") String birthdayFrom,
+                                            @DefaultValue(defaultString) @QueryParam("birthdayto") String birthdayTo) {
+        if (isDefaultValue(firstName, lastName, birthday, birthdayFrom, birthdayTo)) {
+            return studentDAO.getStudentsList();
+        } else {
+            //todo
+        }
+    }
 
     @POST
     @Path("/students")
@@ -71,5 +79,11 @@ public class StudentService {
         } else {
             throw new NotFoundException(new JsonError("Error", "Student " + index + " not found"));
         }
+    }
+
+    //check if all string are default
+    private boolean isDefaultValue(String s1, String s2, String s3, String s4, String s5) {
+        String result = s1 + s2 + s3 + s4 + s5;
+        return result.equals(defaultString);
     }
 }
